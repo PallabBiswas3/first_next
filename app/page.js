6b8aa8935704162afdfae4a57e88e547d563b3ee
PallabBiswas3 <pallabbiswas891@gmail.com>
2024-05-04 15:04:32 +0530

    blog updating

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
 import Image from "next/image";
 import back_image from "/public/Stars.jpg";
+import "./globals.css";
 const home = () => {
   return (
     <div>
-      <Image src={back_image} alt="logo" className="logo" />
+      <div className="home">
+        <Image src={back_image} alt="logo" className="logo" width={1000}
+          height={600} />
+      </div>
+      <div>
+
+        <h1 className="title">Welcome to Space Blogs</h1>
+        <div className="buttonContainer">
+          <a href="#" className="btnBigRed">Extraterrestrial Life</a>
+          <a href="#" className="btnBigRed">Space Travel</a>
+          <a href="#" className="btnBigRed">Our Universe</a>
+        </div>
+      </div>
     </div>
   );
 };
