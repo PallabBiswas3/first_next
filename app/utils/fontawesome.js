a62daa52c9665a464714c895c35ff3325675970c
PallabBiswas3 <pallabbiswas891@gmail.com>
2024-05-20 09:24:51 +0530

    changing

--- app/utils/fontawesome.js
+++
@@ -1,5 +0,0 @@
-// utils/fontawesome.js
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faArrowRight, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faArrowRight, faArrowLeft);
